847944aab03582e81e8467e4a1f971a10aac3fac
Honza Javorek <mail@honzajavorek.cz>
2019-04-25 17:29:19 +0200

    refactor: rewrite to Node.js

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source "https://rubygems.org"
-
-gem "aruba", "0.6.2"
-gem "cucumber", "~> 1.3.20"
--- features/support/steps.js
+++ features/support/steps.js
@@ -1,19 +1,25 @@
-const { expect } = require('chai');
-const fs = require('fs-extra');
 const os = require('os');
 const path = require('path');
-const which = require('which');
+const util = require('util');
 const childProcess = require('child_process');
+const { expect } = require('chai');
+const fs = require('fs-extra');
+const net = require('net');
+const which = require('which');
+const kill = require('tree-kill');
 const { Given, When, Then, Before, After } = require('cucumber');
 
 
 Before(function () {
   this.dir = fs.mkdtempSync(path.join(os.tmpdir(), 'dredd-hooks-template-'));
   this.env = { ...process.env };
+  this.commands = [];
+  this.dataSent = '';
 });
 
-After(function () {
+After(async function () {
   fs.remove(this.dir);
+  await util.promisify(kill)(this.proc.pid);
 });
 
 
@@ -38,11 +44,69 @@ When(/^I run `([^`]+)`$/, function (command) {
   });
 });
 
+When(/^I run `([^`]+)` interactively$/, function (command) {
+  this.proc = childProcess.spawn(command, [], {
+    shell: true,
+    cwd: this.dir,
+    env: this.env,
+  });
+});
 
-Then('the exit status should be {int}', function (number) {
-  expect(this.proc.status).to.equal(parseInt(number, 10));
+When('I wait for output to contain {string}', function (output, callback) {
+  const proc = this.proc;
+
+  function read(data) {
+    if (data.toString().includes(output)) {
+      proc.stdout.removeListener('data', read);
+      proc.stderr.removeListener('data', read);
+      callback();
+    }
+  }
+
+  proc.stdout.on('data', read);
+  proc.stderr.on('data', read);
+});
+
+When('I connect to the server', async function () {
+  this.socket = new net.Socket();
+  const connect = util.promisify(this.socket.connect.bind(this.socket));
+  await connect(61321, '127.0.0.1');
+});
+
+When('I send a JSON message to the socket:', function (message) {
+  this.socket.write(message);
+  this.dataSent += message;
+});
+
+When('I send a newline character as a message delimiter to the socket', function () {
+  this.socket.write('\n');
+});
+
+
+Then('the exit status should be {int}', function (status) {
+  expect(this.proc.status).to.equal(status);
 });
 
 Then('the output should contain:', function (output) {
   expect(this.proc.stdout.toString() + this.proc.stderr.toString()).to.contain(output);
+});
+
+Then('it should start listening on localhost port {int}', async function (port) {
+  this.socket = new net.Socket();
+  const connect = util.promisify(this.socket.connect.bind(this.socket));
+  await connect(port, '127.0.0.1'); // throws if there's an issue
+  this.socket.end();
+});
+
+Then('I should receive the same response', function (callback) {
+  this.socket.on('data', (data) => {
+    const dataReceived = JSON.parse(data.toString());
+    const dataSent = JSON.parse(this.dataSent);
+    expect(dataReceived).to.deep.equal(dataSent);
+    callback();
+  });
+});
+
+Then('I should be able to gracefully disconnect', function () {
+  this.socket.end();
 });
