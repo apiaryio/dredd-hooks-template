ccd662c55ba37783293195644a7d30fea4f1c94b
Honza Javorek <mail@honzajavorek.cz>
2019-05-24 12:22:48 +0200

    chore: allow using console

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   extends: 'airbnb-base',
-  env: { 'mocha': true, 'node': true }
+  env: {
+    'mocha': true,
+    'node': true
+  },
+  rules: {
+    // Using 'console' is perfectly okay for a Node.js CLI tool and avoiding
+    // it only brings unnecessary complexity
+    'no-console': 'off',
+  }
 };
