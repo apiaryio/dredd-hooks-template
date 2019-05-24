8bb5ae2cefa635d4d8e9e3f315e701614c960cfc
Honza Javorek <mail@honzajavorek.cz>
2019-05-24 11:57:51 +0200

    refactor: use a better name for an argument

--- cli/copyFeatures.js
+++ cli/copyFeatures.js
@@ -6,13 +6,13 @@ const glob = require('glob');
 /**
  * Copies all '*.feature' files from the 'srcDir' directory to the 'dstDir'
  * directory, allowing for a transformation of the file basename on the way
- * using the 'modifyBasename()' function
+ * using the 'transformBasename()' function
  */
-module.exports = function copyFeatures(srcDir, dstDir, modifyBasename = basename => basename) {
+module.exports = function copyFeatures(srcDir, dstDir, transformBasename = basename => basename) {
   glob.sync(path.join(srcDir, '*.feature')).forEach((featureSrcPath) => {
     const featureContent = fs.readFileSync(featureSrcPath, { encoding: 'utf-8' });
     const featureBasename = path.basename(featureSrcPath);
-    const featurePath = path.join(dstDir, modifyBasename(featureBasename));
+    const featurePath = path.join(dstDir, transformBasename(featureBasename));
     fs.writeFileSync(featurePath, featureContent, { encoding: 'utf-8' });
   });
 }
